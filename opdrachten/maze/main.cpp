e3bdd5c0f97d3d599ac1d1697840bfecddcd2e8d
Jack <jackdijkema@hotmail.com>
2024-12-20 00:57:32 +0100

    init

---
+++ opdrachten/maze/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+int main() {
+
+  int state = 0; 
+  list<int> inventory[6];
+
+
+  if(!state) {
+    cout << "Welcome to the Maze game... \n";
+    cout << "There's some strange rooms... \n";
+    cout << "Find the objects to enter every room, goodluck \n";
+  }
+
+
+
+  return 0;
+}

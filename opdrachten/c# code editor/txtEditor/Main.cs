83872d4ea972f8c455cc9a438936769ff7099d24
Jack <jackdijkema@hotmail.com>
2025-06-08 00:02:09 +0200

    insert and delete text

--- opdrachten/c# code editor/txtEditor/Main.cs
+++ opdrachten/c# code editor/txtEditor/Main.cs
@@ -1,79 +1,136 @@
 ﻿
 
-class TekstClass
+using System.Collections;
+using System.Runtime.InteropServices;
+
+
+string path = args[0];
+string[] lines = File.ReadAllLines(path.ToString());
+int y = lines.Length - 1;
+int x = 0 + 3;
+
+List<string> test = new List<string>();
+
+while (true)
 {
-    static void Main(string[] args)
+
+    RedrawScreen(lines, y, x);
+
+    var key = Console.ReadKey(true);
+
+    if (key.Key == ConsoleKey.RightArrow)
     {
-
-        string path = args[0];
-        string[] lines = File.ReadAllLines(path.ToString());
-        int y = lines.Length - 1;
-        int x = 0 + 3;
-
-        while (true)
+        if (x >= lines[y].Length + 3)
         {
+            x--;
+        }
+        x++;
+        RedrawScreen(lines, y, x);
+    }
 
-            RedrawScreen(lines, y, x);
+    if (key.Key == ConsoleKey.LeftArrow)
+    {
+        if (x <= 3)
+        {
+            x++;
+        }
 
-            var key = Console.ReadKey(true);
+        x--;
+        RedrawScreen(lines, y, x);
+    }
 
-            if (key.Key == ConsoleKey.RightArrow)
+    if (key.Key == ConsoleKey.DownArrow)
+    {
+        if (y >= lines.Length - 1)
+        {
+            y--;
+        }
+
+        if (x == lines[y].Length + 2)
+        {
+            x = lines[y + 1].Length + 2;
+        }
+
+        y++;
+        RedrawScreen(lines, y, x);
+    }
+    if (key.Key == ConsoleKey.UpArrow)
+    {
+        if (y <= 0)
+        {
+            y++;
+        }
+        y--;
+        RedrawScreen(lines, y, x);
+    }
+
+    bool insertMode = false;
+
+    if (key.Key == ConsoleKey.I)
+    {
+        insertMode = true;
+
+        RedrawScreen(lines, y, x);
+        while (insertMode)
+        {
+            Console.Write("\x1b[5 q");
+            key = Console.ReadKey(intercept: true);
+
+            if (key.Key == ConsoleKey.Escape)
             {
-                 if (x >= lines[y].Length + 2)
+                insertMode = false;
+                Console.Write("\x1b[1 q");
+                RedrawScreen(lines, y, x);
+                Save(lines, path);
+                continue;
+            }
+
+            if (key.Key == ConsoleKey.Backspace)
+            {
+                if (x > 0 + 3)
                 {
                     x--;
+                    lines[y] = lines[y].Remove(x - 3, 1);
                 }
-                x++;
+                else
+                {
+                    continue;
+                }
                 RedrawScreen(lines, y, x);
+                continue;
             }
 
-            if (key.Key == ConsoleKey.LeftArrow)
-            {
-                if (x <= 3)
-                {
-                    x++;
-                }
-
-                x--;
-                RedrawScreen(lines, y, x);
-            }
-
-            if (key.Key == ConsoleKey.DownArrow)
-            {
-                if (y >= lines.Length -1)
-                {
-                    y--;
-                }
-
-                if (x == lines[y].Length + 2)
-                {
-                    x = lines[y + 1].Length + 2; 
-                }
-
-                y++;
-                RedrawScreen(lines, y, x);
-            }
-            if (key.Key == ConsoleKey.UpArrow)
-            {
-                if (y <= 0)
-                {
-                    y++;
-                }
-                y--;
-                RedrawScreen(lines, y, x);
-            }
+            string tempLine = lines[y];
+            tempLine = tempLine.Insert(x-3, key.KeyChar.ToString());
+            lines[y] = tempLine;
+            x++;
+            RedrawScreen(lines, y, x);
         }
     }
 
-    static void RedrawScreen(string[] lines, int x, int y)
+}
+
+void RedrawScreen(string[] lines, int x, int y)
+{
+    Console.Clear();
+
+    for (int i = 0; i < lines.Length; i++)
     {
-        Console.Clear();
-
-        for (int i = 0; i < lines.Length; i++)
-        {
-            Console.WriteLine($"{i + 1}: {lines[i]}");
-        }
-
-        Console.SetCursorPosition(y, x);
+        Console.WriteLine($"{i + 1}: {lines[i]}");
     }
+
+    Console.SetCursorPosition(y, x);
+}
+
+void Save(string[] lines, string path)
+{
+    StreamWriter newFile2 = File.CreateText(path);
+    foreach (var line in lines)
+    {
+        newFile2.WriteLine(line);
+    }
+    newFile2.Close();
+
+    Console.Clear();
+    Console.Write("\n Saving... \n \n");
 }
